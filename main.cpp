61a9bc78ac72ab9f1cfff01d8e056c17f8821f49
Thomas(a.k.a. CatArtik) <74744603+Safariminer@users.noreply.github.com>
2021-11-18 19:48:09 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+int main(){
+float speed = 10f;
+sun.speed(speed);
+
+// to uncomment tomorrow
+
+/*
+
+if(sun.ontopof(Erik)){
+sun.slowdown();
+}
+
+if(sun.ontopof(Thomas)){
+sun.speedup();
+}
+*/
+}
